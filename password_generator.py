24700776ebc6c8726259471d7068ecc03cb5113a
raylon <raylon.f@hotmail.com>
2022-01-13 12:22:36 -0300

    Hangman_game feito

--- password_generator.py
+++ password_generator.py
@@ -31,33 +31,13 @@ class PasswordGenerator:
         return passwd
     
     
-'''passw = []
-letters = ''
-symbols = ''
-numbers = '''''
-
 print('---------WELCOME PASSWORD GENERATOR ----------')
 print('')
 print('')
 x = int(input('How many letters you want in your password?\n'))
 y = int(input('How many symbols woud you like?\n'))
 z = int(input('How many numbers woud you like?\n'))
-'''
-for c in range (0, x):
-    letters = random.choice(string.ascii_letters)
-    passw.append(letters)
 
-for c in range(0, y):
-    symbols = random.choice(string.punctuation)
-    n = random.randint(0, len(passw))
-    passw.insert(n, symbols)
-
-for c in range(0, z):
-    numbers = random.randint(0, 9)
-    n = random.randint(0, len(passw))
-    passw.insert(n, str(numbers))
-    
-passwd = ''.join(passw)'''
 
 s = PasswordGenerator()
 ax = s.generator(qntLetters= x, qntSymbols= y, qntNumbers= z)
