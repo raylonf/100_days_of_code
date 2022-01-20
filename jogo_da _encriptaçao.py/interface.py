369607d6e83db9ee55ad15cc9affabc50fc85efe
raylon <raylon.f@hotmail.com>
2022-01-20 18:34:50 -0300

    implantando interface no ceasar chipher

---
+++ jogo_da _encriptaçao.py/interface.py
@@ -0,0 +1,48 @@
+from asyncio import events
+from socket import timeout
+from PySimpleGUI import PySimpleGUI as sg
+from encrypt_decrypt_ceapher import *
+
+
+#Layout
+sg.theme('Reddit')
+layout = [[sg.Text('Type your message:'), sg.Input(key='message', size=(51,2))],
+          [sg.Text('Type the shift number:'), sg.Input(key= 'shift', size=(10,2))],
+          [sg.Button('Encode'), sg.Button('Decode')],
+          [sg.Text(size=(40,1), key= 'resp')],
+          #[sg.Output(size=(70,3), key='-OUTPUT-')],
+          [sg.Button('Exit')]  
+]
+
+
+#janela
+janela = sg.Window(' Caesar Chipher ', layout)
+
+
+#interações
+
+while True:
+    events, values = janela.read()
+    if events == sg.WINDOW_CLOSED or events == 'Exit':
+        break
+    
+            
+    if events == 'Encode' or events == 'Decode':
+        if events == 'Encode':
+            #values['resp'] = "Here's the encoded result:"
+            values['-OUTPUT-'] = encrypt(message= values['message'], shift= int(values['shift']))
+            janela['resp'].update(f"Here's the encoded result: {values['-OUTPUT-']}", text_color = 'red')
+            #print(values['resp'])
+            #print(values['-OUTPUT-'] )
+            
+        elif events == 'Decode':
+            #values['resp'] = "Here's the decoded result:"
+            values['-OUTPUT-'] = decrypt(message= values['message'], shift= int(values['shift']))
+            janela['resp'].update(f"Here's the decoded result: {values['-OUTPUT-']}", text_color = 'red')
+            #print(values['resp'])
+            #print(values['-OUTPUT-'] )
+            
+      
+       
+              
+
